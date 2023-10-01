abe6d361a3ef18d89e6234d1c240938f28fd1308
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-10-01 22:44:28 +0100

    added script for pascal triangle...

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """
+    This function returns a list
+    of integers that represents the
+    pascal triangle of n:
+       . Returns an empty list if n <= 0
+       . assumes n will be always an integer
+    """
+    triangle = []
+
+    if n <= 0:
+        return []
+
+    for i in range(n):
+        if (i == 0):
+            triangle.append([1])
+        else:
+            cur_row = []
+            for j in range(i + 1):
+                if (j == 0 or j == i):
+                    cur_row.append(1)
+                else:
+                    cur_row.append(triangle[i - 1][j - 1] +
+                                   triangle[i - 1][j])
+
+            triangle.append(cur_row)
+
+    return (triangle)
